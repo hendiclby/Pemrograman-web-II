6ddf5926263d0d93a6db57110f5d0839e7397ede
Hendi Putra <123celiboy@gmail.com>
2023-11-02 22:42:25 +0700

    mengubah nama negara, ibu kota menjadi nama kota dan makanan khas

--- pertemuan4/latihanG.php
+++ pertemuan4/latihanG.php
@@ -1,19 +1,19 @@
 <?php
 
 $negara_info = [
-    ["Negara", "Ibukota", "Kode Telepon"],
-    ["Indonesia", "D.K.I. Jakarta", "+62"],
-    ["Singapura", "Singapura", "+65"],
-    ["Malaysia", "Kuala Lumpur", "+60"],
-    ["Brunei", "Bandar Seri Begawan", "+673"],
-    ["Thailand", "Bangkok", "+66"],
-    ["Laos", "Vientiane", "+856"],
-    ["Philippines", "Manila", "+63"],
-    ["Myanmar", "Naypyidaw", "+95"]
+    ["<h3>Nama kota</h3>", "<h3>makanan khas</h3>", "<h3>Harga</h3>"],
+    ["jakarta", "soto betawi", "Rp 25.000"],
+    ["bandung", "mie kocok", "Rp 15.000"],
+    ["yogyakarta", "gudeg", "Rp 20.000"],
+    ["surabaya", "rawon", "Rp 40.000"],
+    ["bali", "ayam betutu", "Rp 50.000"],
+    ["padang", "rendang", "Rp 25.000"],
+    ["semarang", "lumpia semarang", "Rp 15.000"],
+    ["Ambon", "nasi lapola", "Rp 35.000"]
 ];
 
 
-echo '<table border="1" cellpadding="5" cellspacing="0">';
+echo '<table border="1" cellpadding="2" cellspacing="0">';
 foreach ($negara_info as $row) {
     echo '<tr>';
     foreach ($row as $cell) {
